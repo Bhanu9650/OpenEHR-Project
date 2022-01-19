adf8ca9d101bb6d2218cf6c1389207573e509b61
shruti-17 <52755579+shruti-17@users.noreply.github.com>
2022-01-19 20:49:31 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,9 @@
 easy_install pip
 sudo apt install postgresql postgresql-contrib -y
 pip install virtualenv
+sudo apt install git -y
 cd /home/ubuntu/OpenEHR-Project
 python3 -m venv eapr
 source eapr/bin/activate
+git pull origin master
 pip install -r requirements.txt

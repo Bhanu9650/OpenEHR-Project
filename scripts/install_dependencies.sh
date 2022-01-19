e5146f2b72ffb98fa9eef39b8f140e161ca266be
shruti-17 <52755579+shruti-17@users.noreply.github.com>
2022-01-19 18:58:23 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 easy_install pip
 pip install virtualenv
-cd /home/ec2-user/aws-codedeploy
+cd /home/ubuntu/OpenEHR-Project
 virtualenv environment
 source environment/bin/activate
 pip install -r requirements.txt

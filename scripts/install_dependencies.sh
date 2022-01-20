e5c4204bf93005e0ce25b44849a12d2e91c3d1c8
simran-gera <36760642+simran-gera@users.noreply.github.com>
2022-01-20 10:40:42 +0530

    Create install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get install python3-pip
-sudo apt install postgresql postgresql-contrib -y
-pip install virtualenv
-sudo apt install git -y
+# sudo apt-get install python3-pip
+# sudo apt install postgresql postgresql-contrib -y
+# pip install virtualenv
+# sudo apt install git -y
 cd /home/ubuntu/OpenEHR-Project
 # python3 -m venv eapr
 source eapr/bin/activate

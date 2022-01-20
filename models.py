07935802a679ff52403f4a21208c39bd54584deb
Prabhpreet Singh <psinghoff332@gmail.com>
2022-01-21 04:52:07 +0530

    Add migrations files and update model.py
    Changes:
    - Add migrations files with inital migrations (in migrations/version/)
    - Update models.py to add flask_migrate and corresponding code

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from enum import unique
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json, sys
 
 try:
@@ -25,8 +26,12 @@ app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = f"postgresql://{config['DB']['DB_USER']}:{config['DB']['DB_PWD']}@localhost/eapr"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
+
+migrate = Migrate()
+migrate.init_app(app, db)
 
 
 class userdata(db.Model):

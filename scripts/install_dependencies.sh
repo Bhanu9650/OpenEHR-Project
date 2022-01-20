74963faae95847df11a2e99e7ceebea3380629b2
simran-gera <36760642+simran-gera@users.noreply.github.com>
2022-01-20 10:25:29 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,8 @@ sudo apt install postgresql postgresql-contrib -y
 pip install virtualenv
 sudo apt install git -y
 cd /home/ubuntu/OpenEHR-Project
-python3 -m venv eapr
-source eapr/bin/activate
+# python3 -m venv eapr
 git pull origin master
+source eapr/bin/activate
+# pip install -r requirements.txt
+
